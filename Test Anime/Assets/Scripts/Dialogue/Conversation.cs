9d9bc827d6be379732cc884f6287efba64e98bb0
etubig <etubig@uci.edu>
2019-07-14 03:22:46 -0700

    Dialogue editor set up

---
+++ Test Anime/Assets/Scripts/Dialogue/Conversation.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    [CreateAssetMenu(menuName = ("RPG/Conversation"))]
+    public class Conversation : ScriptableObject
+    {
+        [SerializeField] List<ConversationNode> _nodes;
+        public delegate void ValidateDelegate();
+        public event ValidateDelegate onValidated;
+
+        public List<ConversationNode> nodes
+        {
+            get
+            {
+                return _nodes;
+            }
+        }
+
+        public void AddNode(Vector2 posittion)
+        {
+            Undo.RecordObject(this, "Add node.");
+
+            var node = new ConversationNode();
+            node.position = posittion;
+            nodes.Add(node);
+
+            OnValidate();
+        }
+
+        public ConversationNode GetNodeByUUID(string UUID)
+        {
+            foreach(var node in _nodes)
+            {
+                if (node.UUID == UUID)
+                    return node;
+            }
+            return null;
+        }
+
+        public void DeleteNode(ConversationNode node)
+        {
+            Undo.RecordObject(this, "Delete node.");
+            _nodes.Remove(node);
+            OnValidate();
+        }
+
+        public ConversationNode GetRootNode()
+        {
+            var rootNodes = new Dictionary<string, ConversationNode>();
+            foreach(var node in _nodes)
+            {
+                rootNodes[node.UUID] = node;
+            }
+
+            foreach(var node in _nodes)
+            {
+                foreach(var child in node.children)
+                {
+                    if(rootNodes.ContainsKey(child))
+                    {
+                        rootNodes.Remove(child);
+                    }
+                }
+            }
+
+            foreach(var item in rootNodes)
+            {
+                return item.Value;
+            }
+
+            return null;
+        }
+
+        void RemoveNonExistentChildLinks(HashSet<string> UUIDs)
+        {
+            foreach(var node in _nodes)
+            {
+                var childrenCopy = node.children.ToArray();
+                foreach (var child in childrenCopy)
+                {
+                    if(!UUIDs.Contains(child))
+                    {
+                        node.children.Remove(child);
+                        EditorUtility.SetDirty(this);
+                    }
+                }
+            }
+        }
+
+        private HashSet<string> AssignUUIDs()
+        {
+            var UUIDs = new HashSet<string>();
+            foreach(var node in _nodes)
+            {
+                while (UUIDs.Contains(node.UUID) || node.UUID == "")
+                {
+                    node.UUID = System.Guid.NewGuid().ToString();
+                    EditorUtility.SetDirty(this);
+                }
+                UUIDs.Add(node.UUID);
+            }
+            return UUIDs;
+        }
+
+        void OnValidate()
+        {
+            HashSet<string> UUIDs = AssignUUIDs();
+
+            RemoveNonExistentChildLinks(UUIDs);
+
+            onValidated?.Invoke();
+        }
+    }
+}
+
---
+++ Test Anime/Assets/Scripts/Dialogue/ConversationNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    [System.Serializable]
+    public class ConversationNode
+    {
+        public string UUID;
+        public Vector2 position;
+        public string text;
+        public List<string> children = new List<string>();
+        public string actionToTrigger;
+    }
+}
+
+
